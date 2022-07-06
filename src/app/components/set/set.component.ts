5e8a82273bd61b5e12020689f84a58ac3635cd85
Bryan Higgz <bryan.kahiga@gmail.com>
2022-07-06 19:05:12 +0300

    refactored the sets component

--- src/app/components/set/set.component.ts
+++ src/app/components/set/set.component.ts
@@ -7,8 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SetComponent implements OnInit {
   @Input() set: any;
+  noImage: boolean = false;
 
   constructor() {}
 
   ngOnInit() {}
+
+  loadError(event) {
+    this.noImage = true;
+    this.set.icon_svg_uri =
+      'https://c2.scryfall.com/file/scryfall-symbols/sets/default.svg?1643000400';
+  }
 }
