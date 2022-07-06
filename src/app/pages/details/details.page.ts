ac1918f5d35f688f98285cd8417f673a63b36b3c
Bryan Higgz <bryan.kahiga@gmail.com>
2022-07-06 23:10:57 +0300

    added details page logic and header

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  id: string;
+  set$: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private dataService: DataService) {
+    this.id = JSON.parse(this.route.snapshot.paramMap.get('uri'));
   }
 
+  ngOnInit() {
+    this.getSets(this.id);
+  }
+
+  getSets = async (id: string) => {
+    this.set$ = this.dataService.getSpecificSet(id);
+
+    this.set$.subscribe((setsData: any) => {
+      console.log(setsData);
+    });
+  };
 }
