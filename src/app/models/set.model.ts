0151582fd6179bab1e033ead7bf762b0545f292c
Bryan Higgz <bryan.kahiga@gmail.com>
2022-07-06 16:58:39 +0300

    created set model

---
+++ src/app/models/set.model.ts
@@ -0,0 +1,21 @@
+export class Set {
+  card_count: number;
+  code: string;
+  color?: string;
+  digital: boolean;
+  foil_only: boolean;
+  icon_svg_uri: string;
+  id: string;
+  name: string;
+  nonfoil_only: boolean;
+  object: string;
+  released_at: string;
+  scryfall_uri: string;
+  search_uri: string;
+  set_type: string;
+  tcgplayer_id?: number;
+  uri: string;
+  parent_set_code?: string;
+  mtgo_code?: string;
+  arena_code?: string;
+}
