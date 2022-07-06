ceddb542162d561ccc28ac2f1416ce616ef9441d
Bryan Higgz <bryan.kahiga@gmail.com>
2022-07-06 18:08:42 +0300

    mocked the data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { mockSetsData } from '../services/mockData';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getSets = () => {
-    const setsObservable = new Observable((observer) => {
-      setTimeout(() => {
-        observer.next(mockSetsData);
-      }, 3000);
-    });
+    return this.http
+      .get('assets/mockData.json')
+      .pipe(map((res: Response) => res));
 
-    return setsObservable;
+    // const setsObservable = new Observable((observer) => {
+    //   setTimeout(() => {
+    //     observer.next(mockSetsData);
+    //   }, 3000);
+    // });
+
+    // return setsObservable;
   };
 }
