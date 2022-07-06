80c69a4aaffa44364555efe0bc1499108b33027b
Bryan Higgz <bryan.kahiga@gmail.com>
2022-07-06 16:59:04 +0300

    created the data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { mockSetsData } from '../services/mockData';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor() {}
 
-  constructor() { }
+  getSets = () => {
+    const setsObservable = new Observable((observer) => {
+      setTimeout(() => {
+        observer.next(mockSetsData);
+      }, 3000);
+    });
+
+    return setsObservable;
+  };
 }
