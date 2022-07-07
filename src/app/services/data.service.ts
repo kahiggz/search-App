d0ce6b679b2b88aeb3811ff4c63aa59cc2afeeca
Bryan Higgz <bryan.kahiga@gmail.com>
2022-07-07 09:06:13 +0300

    data service refactor

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,15 +11,15 @@ import { Set } from '../models/set.model';
 export class DataService {
   constructor(private http: HttpClient) {}
 
-  getSets = (): Observable<Set[]> => {
+  getSets(): Observable<Set[]> {
     return this.http
       .get('assets/mockData.json')
       .pipe(map((sets: Set[]) => sets));
-  };
+  }
 
-  getSpecificSet = (id: string): Observable<Set[]> => {
+  getSpecificSet(id: string): Observable<Set[]> {
     return this.http
       .get('assets/mockData.json')
       .pipe(map((sets: Set[]) => sets.filter((set: Set) => set.id === id)));
-  };
+  }
 }
