96fc1d6145a9508721a6b62f6322ee414755d7d8
Bryan Higgz <bryan.kahiga@gmail.com>
2022-07-17 22:25:57 +0300

    refactor from map to tap

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { filter, find, map } from 'rxjs/operators';
+import { filter, find, map, tap } from 'rxjs/operators';
 import { Set } from '../models/set.model';
 
 @Injectable({
@@ -13,7 +13,7 @@ export class DataService {
   getSets(): Observable<Set[]> {
     return this.http
       .get('assets/mockData.json')
-      .pipe(map((sets: Set[]) => sets));
+      .pipe(tap((sets: Set[]) => sets));
   }
 
   getSpecificSet(id: string): Observable<Set[]> {
